71a856e8c2c000b3abc293eea5b208d1c68332a8
Multach <matt.multach87@gmail.com>
2020-09-09 12:38:48 -0700

    Remove nfold specification from lasso model

--- Model_Application/Single_Dataset/Lasso.R
+++ Model_Application/Single_Dataset/Lasso.R
@@ -4,17 +4,15 @@ library(glmnet)
 #load data
 #data.full <- readRDS()
 single.data <- data.full[[1]]
+X <- single.data$X
+Y <- single.data$Y
+p <- single.data$conditions$p
 
 #train lasso
 set.seed(1)
 lambda.lasso.try = seq(0.01,0.6,length.out=100)
-train <- sample(1:nrow(single.data$X) , ceiling(nrow(single.data$X) / 2))
-pred_train <- single.data$X[train , ]
-pred_test <- single.data$X[-train , ]
-y_train <- single.data$Y[train]
-y_test <- single.data$Y[-train]
-lam <- cv.glmnet(pred_train , y_train , alpha = 1 , 
-                 lambda = lambda.lasso.try , 
-                 nfolds = floor(nrow(pred_train) / 3))$lambda.min
-lasso_best <- glmnet()
-                 
+lasso.model <- cv.glmnet(X , Y , family = "gaussian" ,
+                         lambda = lambda.lasso.try)
+lambda.lasso.opt <- lasso.model$lambda.min
+lasso.coefs <- predict(lasso.model , type = "coefficients" ,
+        s = lambda.lasso.opt)[2 : (p + 1)]
