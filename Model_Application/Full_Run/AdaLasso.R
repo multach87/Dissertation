8604e930044aa01626c8a0ca9caee14cd404809f
multach87 <35509942+multach87@users.noreply.github.com>
2020-09-21 11:52:01 -0700

    -Finalize scripts for full runs of standard, adaptive, and LAD lasso models

---
+++ Model_Application/Full_Run/AdaLasso.R
@@ -0,0 +1,165 @@
+#load libraries
+library(quantreg)
+library(glmnet)
+library(magrittr)
+library(purrr)
+
+#load data
+#data.full <- readRDS()
+full.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/fulldata_091620.RData")
+
+
+#adaptive lasso function with two-way CV for selecting both lambda and nu/gamma
+adalasso.sim.fnct <- function(data) { 
+       #create simulation tracker
+       tracker <- as.vector(unlist(data$conditions)) 
+       #print tracker of status
+       cat("n = " , tracker[1] , " , p = " , tracker[2] ,
+           " , eta.x = " , tracker[3] , " , eta.y = " , tracker[4] ,
+           " , g = " , tracker[5] , " , h = " , tracker[6] ,
+           ";\n")
+       #load X, Y, and p
+       X <- data$X
+       Y <- data$Y
+       p <- data$conditions$p
+       seed.ridge <- data$seeds[ , "seed.2"]
+       set.seed(seed.ridge)
+       #ridge coefs for weighting
+       lambda.try <- exp(seq(log(0.01) , log(1400) , length.out = 100))
+       ridge.model <- cv.glmnet(x = X , y = Y , lambda = lambda.try , alpha = 0)
+       lambda.ridge.opt <- ridge.model$lambda.min
+       best.ridge.coefs <- predict(ridge.model , type = "coefficients" ,
+                                   s = lambda.ridge.opt)[-1]
+       ##grid of nu/gamma values to try
+       nu.try <- exp(seq(log(0.01) , log(10) , length.out = 100))
+       seed.pre.nu <- data$seeds[ , "seed.3"]
+       set.seed(seed.pre.nu)
+       seed.nu <- sample(rnorm(n = 1000000000) , size = length(nu.try) , replace = FALSE)
+       ##initialize list of best adalasso results from each nu/gamma
+       adalasso.nu.cv <- list()
+       for(i in 1:length(nu.try)) {
+              seed <- seed.nu[i]
+              set.seed(seed)
+              #single adaptive lasso run with ridge weighting and nu = 1
+              adalasso.model <- cv.glmnet(X , Y , family = "gaussian" ,
+                                          lambda = lambda.try , 
+                                          penalty.factor = 1 / abs(best.ridge.coefs)^nu.try[i])
+              lambda.adalasso.opt <- adalasso.model$lambda.min
+              best.adalasso.coefs <- predict(adalasso.model , type = "coefficients" ,
+                                             s = lambda.adalasso.opt)[-1]
+              adalasso.nu.cv[[i]] <- list(model = list(full.model = adalasso.model , 
+                                                       lambda = lambda.adalasso.opt , 
+                                                       coefs = best.adalasso.coefs) , 
+                                          metrics_and_info = list(model.seed.ridge = seed.ridge ,
+                                                                  model.seed.prenu = seed.pre.nu , 
+                                                                  model.seed.nu = seed ,
+                                                                  ridge.coefs = best.ridge.coefs ,
+                                                                  weights = 1 / abs(best.ridge.coefs)^nu.try[i] , 
+                                                                  nu = nu.try[i] , 
+                                                                  lambda = lambda.adalasso.opt , 
+                                                                  coefs = best.adalasso.coefs , 
+                                                                  mpe = adalasso.model$cvm[which(adalasso.model$lambda == lambda.adalasso.opt)] , 
+                                                                  mpe.sd = adalasso.model$cvsd[which(adalasso.model$lambda == lambda.adalasso.opt)] , 
+                                                                  fpr = length(which(best.adalasso.coefs[c(5:p)] != 0)) / length(best.adalasso.coefs[c(5:p)]) , 
+                                                                  fnr = length(which(best.adalasso.coefs[c(1:4)] == 0)) / length(best.adalasso.coefs[1:4])))
+       }
+       #find minimizing nu/gamma
+       adalasso.nu.cv.mpe <- numeric()
+       adalasso.seeds.ridge <- numeric()
+       adalasso.seeds.prenu <- numeric()
+       adalasso.seeds.nu <- numeric()
+       for(i in 1:length(adalasso.nu.cv)) {
+              adalasso.nu.cv.mpe[i] <- adalasso.nu.cv[[i]]$metrics_and_info$mpe
+              adalasso.seeds.ridge[i] <- adalasso.nu.cv[[i]]$metrics_and_info$model.seed.ridge
+              adalasso.seeds.prenu[i] <- adalasso.nu.cv[[i]]$metrics_and_info$model.seed.prenu
+              adalasso.seeds.nu[i] <- adalasso.nu.cv[[i]]$metrics_and_info$model.seed.nu
+       }
+
+       #return(adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]])
+       #store BEST adalasso result plus all seeds
+       ###below is used to check that seeds are regenerated properly and not uniform
+       return(list(mpes = adalasso.nu.cv.mpe , 
+                   seeds.ridge = adalasso.seeds.ridge , 
+                   seeds.prenu = adalasso.seeds.prenu , 
+                   seeds.nu = adalasso.seeds.nu ,  
+                   model = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]] , 
+                   important = list(diagnostics = data.frame(cbind(data.seed = tracker[7] ,
+                                                                   model.seed.ridge = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.ridge ,
+                                                                   model.seed.prenu = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.prenu , 
+                                                                   model.seed.nu = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.nu)) , 
+                                    coefs = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$coefs , 
+                                    weights = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$weights ,
+                                    info = data.frame(cbind(n = tracker[1] , 
+                                                            p = tracker[2] , 
+                                                            eta.x = tracker[3] , 
+                                                            eta.y = tracker[4] , 
+                                                            g = tracker[5] , 
+                                                            h = tracker[6] , 
+                                                            data.seed = tracker[7] ,
+                                                            model.seed.ridge = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.ridge , 
+                                                            model.seed.prenu = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.prenu , 
+                                                            model.seed.nu = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$model.seed.nu , 
+                                                            lambda = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$lambda ,
+                                                            nu = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$nu ,
+                                                            mpe = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$mpe , 
+                                                            mpe.sd = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$mpe.sd , 
+                                                            fpr = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$fpr , 
+                                                            fnr = adalasso.nu.cv[[which.min(adalasso.nu.cv.mpe)]]$metrics_and_info$fnr
+                                                            )
+                                                      )
+                                    )
+                   )
+              )
+}
+
+
+#run across full dataset
+#run across full dataset
+adalasso.full <- full.data %>%   
+       map(safely(adalasso.sim.fnct))
+
+saveRDS(adalasso.full , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Full_results/adalasso_full.RData")
+
+
+{#dealing with error/result from map(safely())
+#create empty lists for error + result
+#adalasso.error <- list()
+#adalasso.result <- list()
+#adalasso.final <- list()
+#split data into separate error and result lists
+#for(i in 1:length(adalasso.full)) { 
+       #iteration tracker
+#       cat("i = " , i , "\n")
+       #fill error list
+#       adalasso.error[[i]] <- list(error = adalasso.full[[i]]$error , 
+#                                   condition = as.data.frame(unlist(testing10.data[[i]]$condition) , 
+#                                                             n = n , p = p , 
+#                                                             eta.x = eta.x , eta.y = eta.y , 
+#                                                             g = g , h = h , seed = seed))
+       #fill in results if results aren't NULL from safely()
+#       adalasso.result[[i]] <- adalasso.full[[i]]$result
+       #fill final list
+#       if(!is.null(adalasso.full[[i]]$result)) {
+#              adalasso.final[[i]] <- adalasso.full[[i]]$result$important
+#       } else {
+#              adalasso.final[[i]] <- adalasso.error[[i]]
+#       }
+#}
+
+#combine diagnostics
+#diagnostics <- data.frame(matrix(ncol = 4 , nrow = length(full.data)))
+#colnames(diagnostics) <- c("data.seed" , "model.seed.ridge" , "model.seed.prenu" , "model.seed.nu")
+#for(i in 1:length(adalasso.final)) {
+#        diagnostics[i , "data.seed"] <- adalasso.final[[i]]$diagnostics$data.seed
+#        diagnostics[i , "model.seed.ridge"] <- adalasso.final[[i]]$diagnostics$model.seed.ridge
+#        diagnostics[i , "model.seed.prenu"] <- adalasso.final[[i]]$diagnostics$model.seed.prenu
+#        diagnostics[i , "model.seed.nu"] <- adalasso.final[[i]]$diagnostics$model.seed.nu
+#}
+
+
+#save files
+#saveRDS(adalasso.result , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Model_Storage/adalasso_result_DEBUG.RData")
+#saveRDS(adalasso.error , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Error_Storage/adalasso_error_DEBUG.RData")
+#saveRDS(adalasso.final , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/adalasso_resultmain_DEBUG.RData")
+#saveRDS(diagnostics , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Diagnostics_Storage/adalasso_diagnostics_DEBUG.RData")
+}
---
+++ Model_Application/Full_Run/Lasso.R
@@ -0,0 +1,82 @@
+#load libraries
+library(glmnet)
+library(magrittr)
+library(purrr)
+
+#load data
+#data.full <- readRDS()
+full.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/fulldata_091620.RData")
+
+
+#lasso application function
+lasso.sim.fnct <- function(data) {
+        #create simulation tracker
+        tracker <- as.vector(unlist(data$conditions)) 
+        
+        #print tracker of status
+        cat("n = " , tracker[1] , " , p = " , tracker[2] ,
+            " , eta.x = " , tracker[3] , " , eta.y = " , tracker[4] ,
+            " , g = " , tracker[5] , " , h = " , tracker[6] ,
+            ";\n")
+      
+       #load X, Y, p, n
+       X <- data$X
+       Y <- data$Y
+       p <- data$conditions$p
+       n <- length(Y)
+       
+       #set seed for generating ridge coefficients for weighting
+       seed.model <- data$seeds[ , "seed.6"]
+       set.seed(seed.model)
+       
+       #set grid of lambda values
+       lambda.lasso.try <- exp(seq(log(0.01) , log(1400) , length.out = 100))
+       #lasso model
+       lasso.model <- cv.glmnet(X , Y , family = "gaussian" ,
+                                lambda = lambda.lasso.try)
+       lambda.lasso.opt <- lasso.model$lambda.min
+       lasso.coefs <- predict(lasso.model , type = "coefficients" ,
+                              s = lambda.lasso.opt)[-1]
+       #initialize important info dataframe
+       #put conditions, model info, and metrics into list
+       return(list(seed.model = seed.model ,  
+                   model = list(full.model = lasso.model , 
+                                lambda = lambda.lasso.opt , coefs = lasso.coefs) , 
+                   metrics = list(mpe = lasso.model$cvm[which(lasso.model$lambda == lambda.lasso.opt)] , 
+                                   mpe.sd = lasso.model$cvsd[which(lasso.model$lambda == lambda.lasso.opt)] , 
+                                   fpr = length(which(lasso.coefs[c(5:p)] != 0)) / length(lasso.coefs[c(5:p)]) , 
+                                   fnr = length(which(lasso.coefs[c(1:4)] == 0)) / length(lasso.coefs[1:4])) , 
+                   important = list(diagnostics = data.frame(cbind(data.seed = tracker[7] ,
+                                                                   model.seed = seed.model)) , 
+                                    coefs = lasso.coefs , 
+                                    info = data.frame(cbind(n = tracker[1] , 
+                                                            p = tracker[2] , 
+                                                            eta.x = tracker[3] , 
+                                                            eta.y = tracker[4] , 
+                                                            g = tracker[5] , 
+                                                            h = tracker[6] , 
+                                                            data.seed = tracker[7] ,
+                                                            model.seed = seed.model , 
+                                                            lambda = lambda.lasso.opt , 
+                                                            mpe = lasso.model$cvm[which(lasso.model$lambda == lambda.lasso.opt)] , 
+                                                            mpe.sd = lasso.model$cvsd[which(lasso.model$lambda == lambda.lasso.opt)] , 
+                                                            fpr = length(which(lasso.coefs[c(5:p)] != 0)) / length(lasso.coefs[c(5:p)]) , 
+                                                            fnr = length(which(lasso.coefs[c(1:4)] == 0)) / length(lasso.coefs[1:4])
+                                                            )
+                                                      )
+                                    )
+                                    )
+                   )                                 
+
+}
+
+
+
+
+#run across full dataset
+lasso.full <- full.data %>%   
+       map(safely(lasso.sim.fnct))
+
+saveRDS(lasso.full , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Full_results/lasso_full.RData")
+
+
