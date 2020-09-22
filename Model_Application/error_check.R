ba82d613d34b2a995c1bbfc6684ff04d0dccd9f6
multach87 <35509942+multach87@users.noreply.github.com>
2020-09-22 13:07:05 -0700

    -Add Huber and OSPLUS to error-checking script

--- Model_Application/error_check.R
+++ Model_Application/error_check.R
@@ -19,3 +19,20 @@ for(i in 1:length(lasso.error)) {
   }
 }
 
+
+#OSLassoPLUS: errors at c(92, 96 , 159)
+OS.error <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Error_Storage/OS_error_DEBUG.RData")
+for(i in 1:length(OS.error)) {
+  if(!is.null(OS.error[[i]]$error)) {
+    cat("error at i = " , i , "\n")
+  }
+}
+
+#Huber lasso: NULL
+Huber.error <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Error_Storage/Huber_error_debug.RData")
+for(i in 1:length(Huber.error)) {
+  if(!is.null(Huber.error[[i]]$error)) {
+    cat("error at i = " , i , "\n")
+  }
+}
+
