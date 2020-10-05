d5fd061e0f99240bc9e2538df23cd3d090c275b2
multach87 <35509942+multach87@users.noreply.github.com>
2020-10-05 10:36:07 -0700

    Update full-run scripts for Adaptive/LAD/OSHuber Lasso and standard ELNet to run on 500-iteration dataset

---
+++ Model_Application/Full_Run/ELNet9_500.R
@@ -0,0 +1,102 @@
+#libraries
+library(glmnet)
+library(purrr)
+library(magrittr)
+library(pense)
+
+#load data
+#data.full <- readRDS()
+#full.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/")
+half.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/500_data_10052020.RData")
+
+#ebug.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/debug_data_091720.RData")
+#testing10.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/testing10_data_091720.RData")
+#single.data <- testing10.data[[1]]
+
+#function
+elnet9.sim.fnct <- function(data , alpha = 0.9) {
+  #create simulation tracker
+  tracker <- as.vector(unlist(data$conditions)) 
+  
+  #print tracker of status
+  cat("n = " , tracker[1] , " , p = " , tracker[2] ,
+      " , eta.x = " , tracker[3] , " , eta.y = " , tracker[4] ,
+      " , g = " , tracker[5] , " , h = " , tracker[6] ,
+      ";\n")
+  
+  #load X, Y, n, p
+  X <- data$X
+  Y <- data$Y
+  n <- length(Y)
+  p <- data$conditions$p
+  
+  #set seed
+  seed.elnetcv <- data$seeds[ , "seed.13"]
+  
+  #set possible lambda values
+  lambda.lasso.try <- seq(log(0.01) , log(1400) , length.out = 100)
+  lambda.lasso.try <- exp(lambda.lasso.try)
+  
+  set.seed(seed.elnetcv)
+  elnet.cv <- pense_cv(x = X , y = Y , alpha = alpha , 
+                       cv_k = 5 , cv_repl = 10 , cv_metric = "rmspe" , 
+                       lambda = lambda.lasso.try , 
+                       intercept = FALSE)
+  #coefficients
+  elnet.coefs <- coef(elnet.cv)[-1]
+  
+  #fit/pred values
+  Y.fit<- X %*% elnet.coefs
+  
+  #store number of nonzero coefs
+  st.elnet <- sum(elnet.coefs != 0)                                         # number nonzero
+  
+  #generate MSE and sd(MSE) for model
+  n <- nrow(X)
+  mse.elnet <- sum((Y - Y.fit) ^ 2) / (n - st.elnet - 1)
+  sd.mse.elnet <- sd((Y - Y.fit) ^ 2 / (n - st.elnet - 1))
+  
+  #store minimizing lambda
+  lambda.elnet.opt <- elnet.cv$cvres$lambda[which.min(elnet.cv$cvres$cvavg)]
+  
+  #store alpha
+  alpha <- alpha
+  
+  return(list(full = elnet.cv , 
+              model = list(coefficient = elnet.coefs , 
+                           fit = Y.fit , 
+                           mpe = mse.elnet , 
+                           mpe.sd = sd.mse.elnet ,
+                           lambda.opt = lambda.elnet.opt) , 
+              important = list(diagnostics = data.frame(cbind(data.seed = tracker[7] , 
+                                                              model.seed.elnet = seed.elnetcv)
+              ) , 
+              coefs = elnet.coefs , 
+              info = data.frame(cbind(n = tracker[1] , 
+                                      p = tracker[2] , 
+                                      eta.x = tracker[3] , 
+                                      eta.y = tracker[4] , 
+                                      g = tracker[5] , 
+                                      h = tracker[6] , 
+                                      data.seed = tracker[7] ,
+                                      model.seed.elnet = seed.elnetcv , 
+                                      lambda.elnet = lambda.elnet.opt , 
+                                      alpha = alpha , 
+                                      mpe = mse.elnet , 
+                                      mpe.sd = sd.mse.elnet ,
+                                      fpr = length(which(elnet.coefs[c(5:p)] != 0)) / length(elnet.coefs[c(5:p)]) , 
+                                      fnr = length(which(elnet.coefs[c(1:4)] == 0)) / length(elnet.coefs[1:4]))
+              )
+              )
+  )
+  )
+  
+  
+}
+
+elnet9.half <- half.data %>%   
+  map(safely(elnet9.sim.fnct))
+
+saveRDS(elnet9.half , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Full_results/elnet9_500.RData")
+
+
---
+++ Model_Application/Full_Run/LADLasso_500.R
@@ -0,0 +1,217 @@
+#load libraries
+library(quantreg)
+library(glmnet)
+library(magrittr)
+library(purrr)
+
+#load data
+#data.half <- readRDS()
+#full.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/fulldata_091620.RData")
+half.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/500_data_10052020.RData")
+
+
+#LAD lasso function with two-way CV for selecting both lambda and nu/gamma
+ladlasso.sim.fnct <- function(data) {
+       #create simulation tracker
+       tracker <- as.vector(unlist(data$conditions)) 
+       
+       #print tracker of status
+       cat("n = " , tracker[1] , " , p = " , tracker[2] ,
+           " , eta.x = " , tracker[3] , " , eta.y = " , tracker[4] ,
+           " , g = " , tracker[5] , " , h = " , tracker[6] ,
+           ";\n")
+       
+       #load X, Y, p, n
+       X <- data$X
+       Y <- data$Y
+       p <- data$conditions$p
+       n <- length(Y)
+       
+       #set seed for generating ridge coefficients for weighting
+       seed.ridge <- data$seeds[ , "seed.4"]
+       set.seed(seed.ridge)
+       
+       #set possible lambda and nu/gamma values
+       lambda.try <- seq(log(0.01) , log(1400) , length.out = 100)
+       lambda.try <- exp(lambda.try)
+       nu.try <- exp(seq(log(0.01) , log(10) , length.out = 100))
+       
+       #set seed for generating nu/gamma for weighting
+       seed.pre.nu <- data$seeds[ , "seed.5"]
+       set.seed(seed.pre.nu)
+       seed.nu <- sample(rnorm(n = 1000000000) , size = length(nu.try) , replace = FALSE)
+       
+       #find ridge coefs for adaptive weighting
+       lambda.try <- exp(seq(log(0.01) , log(1400) , length.out = 100))
+       ridge.model <- cv.glmnet(x = X , y = Y , lambda = lambda.try , alpha = 0)
+       lambda.ridge.opt <- ridge.model$lambda.min
+       best.ridge.coefs <- predict(ridge.model , type = "coefficients" ,
+                                   s = lambda.ridge.opt)[-1]
+       
+       ##initialize list of best ladlasso results from each nu/gamma
+       ladlasso.nu.cv <- list()
+       
+       #loop to generate results from each nu/gamma
+       for(i in 1:length(nu.try)) {
+              #set seed for random process
+              seed <- seed.nu[i]
+              set.seed(seed)
+              
+              #set adaptive weights
+              weights <- 1 / (abs(best.ridge.coefs)^nu.try[i])
+              
+              #create empty vector for BIC values for each possible lambda value
+              BIC <- rep(0 , 100)
+              
+              #generate model for each possible lambda value
+              for (k in 1:100){
+                     rqfit <- rq.fit.lasso(X , Y , lambda = lambda.try[k] * weights)
+                     betalad_tmp <- rqfit$coef
+                     betalad_tmp <- betalad_tmp * (betalad_tmp > 1e-8)
+                     mse <- mean(abs(rqfit$resi))
+                     mdsize <- length(which(betalad_tmp != 0))
+                     BIC[k] <- log(mse) + mdsize * log(n) / n
+              }
+              
+              #indicator for BIC-minimizing lambda/model
+              step <- which.min(BIC) 
+              
+              #generate LAD lasso coefficients for minimizing lambda/model
+              betalad <- rq.fit.lasso(X , Y , lambda = lambda.try[step] * weights)$coef
+              ladlasso <- betalad * (betalad > 1e-8)
+              
+              #store best lambda for given nu/gamma
+              lambda.ladlasso.opt <- lambda.try[step]
+              
+              #store coefficients
+              coeff2.lad <- ladlasso              # get rid of intercept
+              
+              #generate y-hats for each observation
+              pred.lad <- X %*% coeff2.lad #+ coeff.lad[1]
+              
+              #store number of nonzero coefs
+              st.lad <- sum(coeff2.lad != 0)                                          # number nonzero
+              
+              #generate MSE and sd(MSE) for model
+              mse.lad <- sum((Y - pred.lad) ^ 2) / (n - st.lad - 1)
+              sd.mse.lad <- sd((Y - pred.lad) ^ 2 / (n - st.lad - 1))
+              
+              #save list of all info from best model
+              ladlasso.nu.cv[[i]] <- list(other.info = list(fit = pred.lad , 
+                                                            st = st.lad) , 
+                                          metrics_and_info = list(BIC.min = min(BIC) , 
+                                                                  which.BIC.min = step , 
+                                                                  model.seed.ridge = seed.ridge ,
+                                                                  model.seed.prenu = seed.pre.nu , 
+                                                                  model.seed.nu = seed ,
+                                                                  ridge.coefs = best.ridge.coefs ,
+                                                                  weights = weights , 
+                                                                  nu = nu.try[i] , 
+                                                                  lambda = lambda.try[step] , 
+                                                                  coefs = coeff2.lad , 
+                                                                  mpe = mse.lad , 
+                                                                  mpe.sd = sd.mse.lad ,
+                                                                  fpr = length(which(coeff2.lad[c(5:p)] != 0)) / length(coeff2.lad[c(5:p)]) , 
+                                                                  fnr = length(which(coeff2.lad[c(1:4)] == 0)) / length(coeff2.lad[1:4])))
+              
+       }
+       #find/store minimizing nu/gamma, seeds, minimized BIC/step
+       ladlasso.nu.cv.mpe <- numeric()
+       ladlasso.seeds.ridge <- numeric()
+       ladlasso.seeds.prenu <- numeric()
+       ladlasso.seeds.nu <- numeric()
+       ladlasso.BIC.mins <- numeric()
+       ladlasso.which.BIC.mins <- numeric()
+       for(i in 1:length(ladlasso.nu.cv)) {
+              ladlasso.nu.cv.mpe[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$mpe
+              ladlasso.seeds.ridge[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$model.seed.ridge
+              ladlasso.seeds.nu[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$model.seed.nu
+              ladlasso.seeds.prenu[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$model.seed.prenu
+              ladlasso.BIC.mins[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$BIC.min
+              ladlasso.which.BIC.mins[i] <- ladlasso.nu.cv[[i]]$metrics_and_info$which.BIC.min
+       }
+       #store BEST ladlasso result plus all seeds
+       ###below is used to check that seeds are regenerated properly and not uniform
+       return(list(BICs = ladlasso.BIC.mins , 
+                   which.BICs = ladlasso.which.BIC.mins ,
+                   mpes = ladlasso.nu.cv.mpe , 
+                   seeds.ridge = ladlasso.seeds.ridge , 
+                   seeds.prenu = ladlasso.seeds.prenu ,
+                   seeds.nu = ladlasso.seeds.nu ,  
+                   model = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]] ,
+                   important = list(diagnostics = data.frame(cbind(data.seed = tracker[7] ,
+                                                                   model.seed.ridge = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.ridge , 
+                                                                   model.seed.prenu = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.prenu , 
+                                                                   model.seed.nu = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.nu)) , 
+                                    coefs = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$coefs , 
+                                    weights = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$weights ,
+                                    info = data.frame(cbind(n = tracker[1] , 
+                                                            p = tracker[2] , 
+                                                            eta.x = tracker[3] , 
+                                                            eta.y = tracker[4] , 
+                                                            g = tracker[5] , 
+                                                            h = tracker[6] , 
+                                                            data.seed = tracker[7] ,
+                                                            model.seed.ridge = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.ridge , 
+                                                            model.seed.prenu = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.prenu , 
+                                                            model.seed.nu = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$model.seed.nu) ,
+                                                            lambda = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$lambda ,
+                                                            nu = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$nu ,
+                                                            mpe = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$mpe , 
+                                                            mpe.sd = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$mpe.sd , 
+                                                            fpr = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$fpr , 
+                                                            fnr = ladlasso.nu.cv[[which.min(ladlasso.nu.cv.mpe)]]$metrics_and_info$fnr)
+                                    )
+                   )
+              )
+}
+
+#run across full dataset
+ladlasso.half <- half.data %>%   
+       map(safely(ladlasso.sim.fnct))
+
+saveRDS(ladlasso.half , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Full_results/ladlasso_500.RData")
+
+
+{
+#dealing with error/result from map(safely())
+#create empty lists for error + result
+##ladlasso.error <- list()
+##ladlasso.result <- list()
+##ladlasso.final <- list()
+#split data into separate error and result lists
+##for(i in 1:length(ladlasso.half)) { 
+       #iteration tracker
+##       cat("i = " , i , "\n")
+       #fill error list
+##       ladlasso.error[[i]] <- list(error = ladlasso.half[[i]]$error , 
+##                                   condition = as.data.frame(unlist(full.data[[i]]$condition) , 
+##                                                             n = n , p = p , 
+##                                                             eta.x = eta.x , eta.y = eta.y , 
+##                                                             g = g , h = h , seed = seed))
+       #fill in results if results aren't NULL from safely()
+##       ladlasso.result[[i]] <- ladlasso.half[[i]]$result
+       #fill final list
+##       if(!is.null(ladlasso.half[[i]]$result)) {
+##              ladlasso.final[[i]] <- ladlasso.half[[i]]$result$important
+##       } else {
+##              ladlasso.final[[i]] <- ladlasso.error[[i]]
+##       }
+##}
+
+#combine diagnostics
+##diagnostics <- data.frame(matrix(ncol = 4 , nrow = length(full.data)))
+##colnames(diagnostics) <- c("data.seed" , "model.seed.ridge" , "model.seed.prenu" , "model.seed.nu")
+##for(i in 1:length(ladlasso.final)) {
+##        diagnostics[i , "data.seed"] <- ladlasso.final[[i]]$diagnostics$data.seed
+##        diagnostics[i , "model.seed.ridge"] <- ladlasso.final[[i]]$diagnostics$model.seed.ridge
+##        diagnostics[i , "model.seed.prenu"] <- ladlasso.final[[i]]$diagnostics$model.seed.prenu
+##        diagnostics[i , "model.seed.nu"] <- ladlasso.final[[i]]$diagnostics$model.seed.nu
+##}
+
+#save files
+##saveRDS(ladlasso.result , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Model_Storage/ladlasso_result_DEBUG.RData")
+##saveRDS(ladlasso.error , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Error_Storage/ladlasso_error_DEBUG.RData")
+##saveRDS(ladlasso.final , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/ladlasso_resultmain_DEBUG.RData")
+##saveRDS(diagnostics , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Diagnostics_Storage/ladlasso_diagnostics_DEBUG.RData")
+}
