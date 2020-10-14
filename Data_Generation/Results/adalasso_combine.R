f39e9fe309372bf45304c4b81a3495a0d31aa19f
multach87 <35509942+multach87@users.noreply.github.com>
2020-10-14 10:40:48 -0700

    Create and upload script for combining split results of adaptive lasso and lad lasso into dataframes for subsequent analysis

--- Data_Generation/Results/adalasso_combine.R
+++ Data_Generation/Results/adalasso_combine.R
@@ -1,21 +1,25 @@
 #load data
-debug.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/debug_data_091720.RData")
-adalasso.final <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/adalasso_resultmain_DEBUG.RData")
+half.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/500_data_10052020.RData")
+adalasso.final <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/adalasso_resultmain_500.RData")
 
 #initialize dataframe
 adalasso.results <- data.frame(matrix(ncol = ncol(adalasso.final[[1]]$info)))
 colnames(adalasso.results) <- colnames(adalasso.final[[1]]$info)
 
 #fill results
+##Errors at: NONE
 for(i in 1:length(adalasso.final)) {
   if(is.null(adalasso.final[[i]]$error)) {
     adalasso.results[i , ] <- adalasso.final[[i]]$info
   } else {
     cat("error at i = " , i , "\n")
-    adalasso.results[i , 1:7] <- debug.data[[i]]$conditions
+    adalasso.results[i , 1:7] <- half.data[[i]]$conditions
   }
 }
 
 mean(adalasso.results[ , "fpr"] , na.rm = T)
 mean(adalasso.results[ , "fnr"] , na.rm = T)
-mean(adalasso.results[ , "mpe"] , na.rm = T)
+mean(adalasso.results[!is.infinite(adalasso.results[ , "mpe"]) , "mpe"] , na.rm = T)
+
+#save results
+saveRDS(adalasso.results , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/adalasso_resultDF_500.RData")
--- Data_Generation/Results/ladlasso_combine.R
+++ Data_Generation/Results/ladlasso_combine.R
@@ -1,21 +1,30 @@
 #load data
-debug.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/debug_data_091720.RData")
-ladlasso.final <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/ladlasso_resultmain_DEBUG.RData")
+half.data <- readRDS("/Users/Matt Multach/Desktop/Dissertation/Dissertation_Git/Data_Generation/Data_Storage/500_data_10052020.RData")
+ladlasso.final <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/ladlasso_resultmain_500.RData")
 
 #initialize dataframe
 ladlasso.results <- data.frame(matrix(ncol = ncol(ladlasso.final[[1]]$info)))
 colnames(ladlasso.results) <- colnames(ladlasso.final[[1]]$info)
 
+#initialize error vector
+ladlasso.errors <- numeric()
+
 #fill results
+##Errors at:
 for(i in 1:length(ladlasso.final)) {
   if(is.null(ladlasso.final[[i]]$error)) {
     ladlasso.results[i , ] <- ladlasso.final[[i]]$info
   } else {
     cat("error at i = " , i , "\n")
-    ladlasso.results[i , 1:7] <- debug.data[[i]]$conditions
+    ladlasso.results[i , 1:7] <- half.data[[i]]$conditions
+    ladlasso.errors <- c(ladlasso.errors , i)
   }
 }
 
 mean(ladlasso.results[ , "fpr"] , na.rm = T)
 mean(ladlasso.results[ , "fnr"] , na.rm = T)
-mean(ladlasso.results[ , "mpe"] , na.rm = T)
+mean(ladlasso.results[!is.infinite(ladlasso.results[ , "mpe"]) , "mpe"] , na.rm = T)
+
+#save results
+saveRDS(ladlasso.results , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/MainResults_Storage/ladlasso_resultDF_500.RData")
+saveRDS(ladlasso.errors , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Error_Storage/ladlasso_errorindices_500.RData")
