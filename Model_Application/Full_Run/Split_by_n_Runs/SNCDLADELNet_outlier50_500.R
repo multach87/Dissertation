5e7e0b29a7d40b4c72099ca6c54527ccc7015f39
multach87 <35509942+multach87@users.noreply.github.com>
2021-02-20 16:26:59 -0800

    Add coefs to HD sims for SNCD-based LAD Elastic Net

--- Model_Application/Full_Run/Split_by_n_Runs/SNCDLADELNet_outlier50_500.R
+++ Model_Application/Full_Run/Split_by_n_Runs/SNCDLADELNet_outlier50_500.R
@@ -7,7 +7,7 @@ library(purrr)
 #load data
 HALF.data <- readRDS("/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Split_by_n/outlier50.RData")
 
-SNCDLAD.elnet.sim.fnct <- function(data) { 
+SNCDLAD.elnet5.sim.fnct <- function(data) { 
   #create simulation tracker
   tracker <- as.vector(unlist(data$conditions)) 
   #print tracker of status
@@ -32,35 +32,35 @@ SNCDLAD.elnet.sim.fnct <- function(data) {
                               s = lambda.ridge.opt)[-1]
   ##grid of nu/gamma values to try
   nu.try <- exp(seq(log(0.01) , log(10) , length.out = 100))
-  ##initialize full list of LAD elnet results from each nu/gamma
-  LADelnet.nu.cv.full <- list()
+  ##initialize full list of LAD elnet5 results from each nu/gamma
+  LADelnet5.nu.cv.full <- list()
   ##initialize matrices of metrics and minimizing results
-  LADelnet.nu.cv.lambda <- numeric()
-  LADelnet.nu.cv.mse <- numeric()
-  LADelnet.nu.cv.msesd <- numeric()
-  LADelnet.nu.cv.coefs <- list()
+  LADelnet5.nu.cv.lambda <- numeric()
+  LADelnet5.nu.cv.mse <- numeric()
+  LADelnet5.nu.cv.msesd <- numeric()
+  LADelnet5.nu.cv.coefs <- list()
   ##Loop over nu/gamma values for CV, storing minimizing lambda within each nu/gamma
   for(i in 1:length(nu.try)) {
-    invisible(capture.output(LADelnet.nu.cv.full[[i]] <- cv.hqreg(X = X , y = Y , method = "quantile" , tau = 0.5 , 
+    invisible(capture.output(LADelnet5.nu.cv.full[[i]] <- cv.hqreg(X = X , y = Y , method = "quantile" , tau = 0.5 , 
                                                                   lambda = lambda.try , alpha = 0.5 , preprocess = "standardize" , 
                                                                   screen = "SR" , penalty.factor = 1 / abs(best.ridge.coefs)^nu.try[i] , 
                                                                   FUN = "hqreg" , type.measure = "mse")))
-    LADelnet.nu.cv.mse[i] <- min(LADelnet.nu.cv.full[[i]]$cve)
-    LADelnet.nu.cv.msesd[i] <- LADelnet.nu.cv.full[[i]]$cvse[which.min(LADelnet.nu.cv.full[[i]]$cve)]
-    LADelnet.nu.cv.lambda[i] <- LADelnet.nu.cv.full[[i]]$lambda.min
-    LADelnet.nu.cv.coefs[[i]] <- LADelnet.nu.cv.full[[i]]$fit$beta[-1 , which.min(LADelnet.nu.cv.full[[i]]$cve)]
+    LADelnet5.nu.cv.mse[i] <- min(LADelnet5.nu.cv.full[[i]]$cve)
+    LADelnet5.nu.cv.msesd[i] <- LADelnet5.nu.cv.full[[i]]$cvse[which.min(LADelnet5.nu.cv.full[[i]]$cve)]
+    LADelnet5.nu.cv.lambda[i] <- LADelnet5.nu.cv.full[[i]]$lambda.min
+    LADelnet5.nu.cv.coefs[[i]] <- LADelnet5.nu.cv.full[[i]]$fit$beta[-1 , which.min(LADelnet5.nu.cv.full[[i]]$cve)]
   }
   
   #specify minimizing nu value and resulting model info
-  nu.opt <- nu.try[which.min(LADelnet.nu.cv.mse)]
-  lambda.opt <- LADelnet.nu.cv.lambda[which.min(LADelnet.nu.cv.mse)]
+  nu.opt <- nu.try[which.min(LADelnet5.nu.cv.mse)]
+  lambda.opt <- LADelnet5.nu.cv.lambda[which.min(LADelnet5.nu.cv.mse)]
   weights.opt <- 1 / abs(best.ridge.coefs)^nu.opt
-  coefs.opt <- LADelnet.nu.cv.coefs[[which.min(LADelnet.nu.cv.mse)]]
-  LADelnet.mse.min <- min(LADelnet.nu.cv.mse)
-  LADelnet.mse.min.se <- LADelnet.nu.cv.msesd[which.min(LADelnet.nu.cv.mse)]
+  coefs.opt <- LADelnet5.nu.cv.coefs[[which.min(LADelnet5.nu.cv.mse)]]
+  LADelnet5.mse.min <- min(LADelnet5.nu.cv.mse)
+  LADelnet5.mse.min.se <- LADelnet5.nu.cv.msesd[which.min(LADelnet5.nu.cv.mse)]
   
-  #return(adaelnet5.nu.cv[[which.min(adaelnet5.nu.cv.mpe)]])
-  #store BEST adaelnet5 result plus all seeds
+  #return(adaelnet55.nu.cv[[which.min(adaelnet55.nu.cv.mpe)]])
+  #store BEST adaelnet55 result plus all seeds
   ###below is used to check that seeds are regenerated properly and not uniform
   return(list(full = list(
                  ridge.coefs = best.ridge.coefs ,
@@ -76,8 +76,8 @@ SNCDLAD.elnet.sim.fnct <- function(data) {
                                  alpha = 0.5 ,
                                  lambda = lambda.opt ,
                                  nu = nu.opt ,
-                                 mpe = LADelnet.mse.min ,
-                                 mpe.sd = LADelnet.mse.min.se ,
+                                 mpe = LADelnet5.mse.min ,
+                                 mpe.sd = LADelnet5.mse.min.se ,
                                  fpr = length(which(coefs.opt[c(5:p)] != 0)) / length(coefs.opt[c(5:p)]) , 
                                  fnr = length(which(coefs.opt[c(1:4)] == 0)) / length(coefs.opt[1:4])
     )
@@ -87,7 +87,7 @@ SNCDLAD.elnet.sim.fnct <- function(data) {
 }
 
 #run across full dataset
-LADelnet.HALF <- HALF.data %>%   
-  map(safely(SNCDLAD.elnet.sim.fnct))
+LADelnet5.HALF <- HALF.data %>%   
+  map(safely(SNCDLAD.elnet5.sim.fnct))
 
-saveRDS(LADelnet.HALF , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Split_by_n/SNCDLADelnet_outlier50_500.RData")
+saveRDS(LADelnet5.HALF , "/Users/Matt Multach/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/Split_by_n/SNCDLADelnet5_outlier50_500_COEFS.RData")
