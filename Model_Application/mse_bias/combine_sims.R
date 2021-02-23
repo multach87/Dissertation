e1e30249b37f00d8e1198ce080d25f946ea4a55d
Multach <matt.multach87@gmail.com>
2021-02-23 10:21:13 -0800

    -Update script for combining sim results

---
+++ Model_Application/mse_bias/combine_sims.R
@@ -0,0 +1,23 @@
+lasso.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_lasso_500.RData")
+lassoHD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_lasso_HD_500.RData")
+
+elnet5.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_elnet5_500.RData")
+elnet5HD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_elnet5_HD_500.RData")
+
+adalasso.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_adalasso_500.RData")
+adalassoHD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_adalasso_HD_500.RData")
+
+adaelnet5.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_adaelnet5_500.RData")
+adaelnet5HD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_adaelnet5_HD_500.RData")
+
+msaelnet5.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_msaelnet5_500.RData")
+msaelnet5HD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_msaelnet5_HD_500.RData")
+
+
+
+
+huberelnet5.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_huberelnet5_500_fprfnr.RData")
+huberlasso.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_huberlasso_500_fprfnr.RData")
+
+oshuberlassoHD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_OShuberlasso_HD_500.RData")
+oslassoHD.msebias <- readRDS("/Users/Matt/Dropbox/USC_Grad2/Courses/Dissertation/Dissertation_Git/Data_Storage/mse_bias/msebias_OSlasso_HD_500.RData")
